a3d01da3612e6e15fe4b619a4d76e7f222240ac1
Sergi <sergi.prof@hotmail.com>
2025-06-01 19:46:11 +0200

    config: creates profiles - .env.development for local development and .env.production

--- src/api-services/axios-instance.tsx
+++ src/api-services/axios-instance.tsx
@@ -1,9 +1,11 @@
 import axios from "axios";
+import { API_BASE_URL } from "../config/config";
 
-const apiUrl = "http://34.205.175.94:8080/api/v1/";
+//const apiUrl = "http://34.205.175.94:8080/api/v1/";
+const apiUrl = import.meta.env.VITE_API_URL || "http://localhost:8080/api/v1/";
 
 const axiosInstance = axios.create({
-  baseURL: apiUrl,
+  baseURL: API_BASE_URL,
   // timeout: 10000,
   headers: {
     "Content-Type": "application/json",
